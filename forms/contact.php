de4041bb2adca9fd33210a790c9cbc04e5efd631
RenderTalentPartners <rendertalentpartners@gmail.com>
2025-04-02 23:21:21 +0530

    updated

--- forms/contact.php
+++ forms/contact.php
@@ -1,4 +1,6 @@
 <?php
+// filepath: c:\Users\Gagan\Documents\GitHub\RenderTalentPartner\forms\contact.php
+
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
